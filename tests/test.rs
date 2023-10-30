01fe697042ec9d89374174f57a5605838bee03e7
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-29 22:41:07 -0400

    Create test.rs

---
+++ tests/test.rs
@@ -0,0 +1,24 @@
+use invidivual_project_kelly_rust::{extract, query, transform_load};
+
+#[test]
+fn test_extract_and_transform_load() {
+    let url = "https://raw.githubusercontent.com/selva86/datasets/master/Auto.csv";
+    let file_path = "Auto_test.csv";
+
+    // Extract
+    extract(url, file_path).unwrap();
+    assert!(std::fs::metadata(file_path).is_ok());
+
+    // Transform and Load
+    let result = transform_load(file_path);
+    assert_eq!(result.unwrap(), "AutoDB.db");
+}
+
+#[test]
+fn test_query() {
+    // Query the top 5 rows from the CarsDB table
+    let result = query("SELECT * FROM AutoDB LIMIT 5;");
+
+    // Check if the query was successful and returns 5 rows
+    assert!(result.is_ok());
+}
