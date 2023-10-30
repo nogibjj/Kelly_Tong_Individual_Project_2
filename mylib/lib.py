b814f354519d99cc9ad635b3084123556f696680
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-10-29 22:36:20 -0400

    Delete mylib directory

--- mylib/lib.py
+++
@@ -1,7 +0,0 @@
-def compute_average(df):
-  average_mpg = df["mpg"].mean()
-  average_weight = df["weight"].mean()
-  return {
-          'weight average': average_weight,
-          'mpg average': average_mpg
-          }
